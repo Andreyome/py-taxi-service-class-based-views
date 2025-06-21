3f7e3b55d9b2a8e1089efd171601b2d418e21b0a
Andrii Omelchenko <andreyomem@gmail.com>
2025-06-22 00:09:02 +0300

    implement views, templates

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,30 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    ManufacturerListView,
+    DriverDetailView,
+    DriverListView,
+    CarListView,
+    CarDetailView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturers/",
+         ManufacturerListView.as_view(),
+         name="manufacturer-list"),
+    path("drivers/", DriverListView.as_view(),
+         name="driver-list"),
+    path("drivers/<int:pk>/",
+         DriverDetailView.as_view(),
+         name="driver-detail"),
+    path("drivers/<int:pk>/",
+         DriverDetailView.as_view(),
+         name="driver_detail"),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
 ]
 
 app_name = "taxi"
